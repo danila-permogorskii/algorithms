fed6ba5ff8668a2374fdceb6c6c64c0ba6a19b60
Danila <55054065+danila-permogorskii@users.noreply.github.com>
2023-08-24 23:13:23 +0300

    init

---
+++ LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,47 @@
+﻿static class Program
+{
+	public static void Main(string[] args)
+	{
+		var str = "pwwkew";
+
+		var res = OtherSolution(str);
+
+		Console.WriteLine(res);
+	}
+
+	static int MySolution()
+	{
+		var str = "pwwkew";
+		var hs = new HashSet<char>();
+
+		for (int i = 0; i < str.Length; i++)
+		{
+			if (!hs.Contains(str[i]))
+				hs.Add(str[i]);
+		}
+
+		return hs.Count;
+	}
+
+	static int OtherSolution(string s)
+	{
+		var charSet = new HashSet<char>();
+		int left = 0, right = 0, maxLength = 0;
+		while(right < s.Length)
+		{
+			if (!charSet.Contains(s[right]))
+			{
+				charSet.Add(s[right]);
+				right++;
+				maxLength = Math.Max(maxLength, charSet.Count);
+			}
+			else
+			{
+				charSet.Remove(s[left]);
+				left++;
+			}
+		}
+		return maxLength;
+	}
+
+}
---
+++ TwoSum/Program.cs
@@ -0,0 +1,29 @@
+﻿static class Program
+{
+	public static void Main()
+	{
+		var nums = new int[] { 2, 7, 11, 15 };
+		var target = 9;
+
+		var res = TwoSum(nums, target);
+
+		Console.WriteLine($"[{res[0]}, {res[1]}]");
+	}
+
+	private static int[] TwoSum(int[] nums, int target)
+	{
+		var dict = new Dictionary<byte, byte>();
+
+		for (byte i = 0; i < nums.Length; i++)
+		{
+			var complement = (byte)(target - nums[i]);
+
+			if (dict.TryGetValue(complement, out var value))
+				return new[] {(int)value, i};
+
+			dict[(byte)nums[i]] = i;
+		}
+
+		return Array.Empty<int>();
+	}
+}
