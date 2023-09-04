cdc0f45a4c35f1875038576550f05f728ee08d23
Danila <developer.permogorsk@gmail.com>
2023-09-04 17:19:35 +0300

    Added new projects with different algorithms

---
+++ MergeSort/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+internal static class Program
+{
+	private static void Merge(int[] arr, int[] aux, int low, int mid, int high)
+	{
+		int k = low, i = low, j = mid + 1;
+
+		while (i <= mid && j <= high)
+		{
+			if (arr[i] <= arr[j])
+				aux[k++] = arr[i++];
+			else
+				aux[k++] = arr[j++];
+		}
+
+		while (i <= mid)
+			aux[k++] = arr[i++];
+
+		for (i = low; i <= high; i++)
+			arr[i] = aux[i];
+	}
+
+	private static void MergeSort(int[] arr, int[] aux, int low, int high)
+	{
+		if (high <= low)
+			return;
+
+		var mid = low + ((high - low) >> 1);
+		
+		MergeSort(arr, aux, low, mid);
+		MergeSort(arr, aux, mid + 1, high);
+		
+		Merge(arr, aux, low, mid, high);
+	}
+
+	private static bool IsSorted(int[] arr)
+	{
+		var prev = arr[0];
+
+		for (int i = 1; i < arr.Length; i++)
+		{
+			if (prev > arr[i])
+			{
+				Console.WriteLine("MergeSort failed.");
+				return false;
+			}
+
+			prev = arr[i];
+		}
+
+		return true;
+	}
+	
+	public static void Main()
+	{
+		var arr = new int[] { 12, 3, 18, 24, 0, 5, -2 };
+		var aux = new int[arr.Length];
+		
+		Array.Copy(arr, aux, arr.Length);
+		
+		MergeSort(arr, aux, 0, arr.Length - 1);
+
+		if (IsSorted(arr))
+			Console.WriteLine(string.Join(", ", arr));
+	}
+}
---
+++ QuickSort/Program.cs
@@ -0,0 +1,49 @@
+﻿internal static class Program
+{
+	private static void Swap(int[] arr, int i, int j)
+	{
+		(arr[i], arr[j]) = (arr[j], arr[i]);
+	}
+
+	private static int Partition(int[] a, int start, int end)
+	{
+		var pivot = a[end];
+
+		var pIndex = start;
+
+		for (int i = start; i < end; i++)
+		{
+			if (a[i] <= pivot)
+			{
+				Swap(a, i, pIndex);
+				pIndex++;
+			}
+		}
+		
+		Swap(a, end, pIndex);
+
+		return pIndex;
+	}
+
+	private static void QuickSort(int[] a, int start, int end)
+	{
+		while (true)
+		{
+			if (start >= end) return;
+
+			var pivot = Partition(a, start, end);
+
+			QuickSort(a, start, pivot - 1);
+			start = pivot + 1;
+		}
+	}
+
+	public static void Main()
+	{
+		var a = new int[] { 9, -3, 5, 2, 6, 8, -6, 1, 3 };
+		
+		QuickSort(a, 0, a.Length - 1);
+
+		Console.WriteLine(a.ToString());
+	}
+}
